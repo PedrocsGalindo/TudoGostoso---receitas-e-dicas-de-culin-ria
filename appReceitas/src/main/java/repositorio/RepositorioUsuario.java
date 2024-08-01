6a62f4f555b6be6922e5fccfa90e3c8c8f011c7f
Pedrocesargs <141459434+Pedrocesargs@users.noreply.github.com>
2024-08-01 19:59:14 -0300

    Continua funcional e herda de generico

--- appReceitas/src/main/java/repositorio/RepositorioUsuario.java
+++ appReceitas/src/main/java/repositorio/RepositorioUsuario.java
@@ -2,68 +2,21 @@ package repositorio;
 
 import controle.Usuario;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import javax.mail.internet.InternetAddress;
 
 
-public class RepositorioUsuario implements IRepositorio<Usuario> {
+public class RepositorioUsuario extends RepositorioGenerico<Usuario> {
 
-    //caminho do arquivo, relativo em relação ao content root
-    private static final Path path = Paths.get("appReceitas/src/main/recursos/repositorios/repositorioUsuarios.ser");
-
-
-    //Busca e retorna a lista de Usuarios, se tiver fazia, retorna uma lista vazia
-    public List<Usuario> buscar(){
-
-        List<Usuario> usuarios = new ArrayList<>();
-        try(ObjectInputStream input = new ObjectInputStream( Files.newInputStream(path))){
-            try{
-                usuarios = (List<Usuario>) input.readObject();
-            }catch (ClassNotFoundException e){
-                System.out.println("erro ao ler arquivo");
-            }
-        }catch (IOException e){
-            System.out.println("Erro ao abrir arquivo");
-        }
-        return usuarios;
+    // Define o caminho do arquivo específico para usuários
+    public RepositorioUsuario() {
+        super("appReceitas/out/production/appReceitas/repositorios/RepositorioUsuarios/repositorio.ser");
     }
 
-    //salva usurio novo
-    public void salvar(Usuario usuario) {
-
-        List<Usuario> usuarios = buscar();
-        usuarios.add(usuario);
-        try(ObjectOutputStream output = new ObjectOutputStream(Files.newOutputStream(path))){
-            output.writeObject(usuarios);
-        }catch (IOException e){
-            System.out.println("Erro ao abrir arquivo para escrever");
-        }
-    }
-
-    //remove usuario existente
-    public void excluir(Usuario usuario) {
-        List<Usuario> usuarios = buscar();
-        int hUsuarios = usuarios.size();
-        usuarios.remove(usuario);
-
-        //verefica se a lista foi alterada, se ela for, significa que existe o usuario e foi excluido
-        if (usuarios.size() != hUsuarios){
-            try(ObjectOutputStream output = new ObjectOutputStream(Files.newOutputStream(path))){
-                output.writeObject(usuarios);
-            }catch (IOException e){
-                System.out.println("Erro ao abrir arquivo para escrever");
-            }
-        }
-    }
-
+    //buscar, salvar e excluir herdados de Generico
 
     //recuperar Usuario com id, se não achar retorna NULL
-    public Usuario recuperar(int id) {
+    public Usuario buscarPorId(int id) {
 
         List<Usuario> usuarios = buscar();
         Usuario usuario = null;
@@ -76,7 +29,7 @@ public class RepositorioUsuario implements IRepositorio<Usuario> {
     }
 
     //recuperar Usuario com cpf, se não achar retorna NULL
-    public Usuario recuperar(String cpf){
+    public Usuario buscarPorCpf(String cpf){
 
         List<Usuario> usuarios = buscar();
         Usuario usuario = null;
@@ -88,7 +41,7 @@ public class RepositorioUsuario implements IRepositorio<Usuario> {
         return usuario;
     }
     //recuperar Usuario com emaill, se não achar retorna NULL
-    public Usuario recuperar(InternetAddress  email){
+    public Usuario buscarPorEmail(InternetAddress  email){
 
         List<Usuario> usuarios = buscar();
         Usuario usuario = null;
