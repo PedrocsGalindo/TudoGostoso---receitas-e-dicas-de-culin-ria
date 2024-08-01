88ffd0e17c9f696e8e3ee95d3a3875b42c64045d
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-08-01 17:09:03 -0300

    Update RepositorioReceita.java
    Atualizando Repositorio Receita

--- appReceitas/src/main/java/repositorio/RepositorioReceita.java
+++ appReceitas/src/main/java/repositorio/RepositorioReceita.java
@@ -1,6 +1,26 @@
 package repositorio;
 
 import controle.Receita;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RepositorioReceita extends RepositorioGenerico<Receita> {
+
+    public List<Receita> buscarPorNome(String nome) {
+        return items.stream()
+                .filter(receita -> receita.getNome().equalsIgnoreCase(nome))
+                .collect(Collectors.toList());
+    }
+
+    public List<Receita> buscarPorAutor(String autor) {
+        return items.stream()
+                .filter(receita -> receita.getAutor().equalsIgnoreCase(autor))
+                .collect(Collectors.toList());
+    }
+
+    public List<Receita> buscarPorAvaliacao(int avaliacao) {
+        return items.stream()
+                .filter(receita -> receita.getAvaliacao() == avaliacao)
+                .collect(Collectors.toList());
+    }
 }
