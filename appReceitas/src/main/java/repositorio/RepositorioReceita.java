9c2beba467900d6f575f4cf767a172126417f4ef
Pedrocesargs <141459434+Pedrocesargs@users.noreply.github.com>
2024-08-01 19:58:47 -0300

    Funcional e herda de generico

--- appReceitas/src/main/java/repositorio/RepositorioReceita.java
+++ appReceitas/src/main/java/repositorio/RepositorioReceita.java
@@ -1,26 +1,36 @@
 package repositorio;
 
+import controle.Avaliacao;
 import controle.Receita;
+import controle.Usuario;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RepositorioReceita extends RepositorioGenerico<Receita> {
 
-    public List<Receita> buscarPorNome(String nome) {
+    public RepositorioReceita() {
+        super("appReceitas/out/production/appReceitas/repositorios/RepositorioReceitas/repositorio.ser");
+    }
+
+    public List<Receita> buscarPorTitulo(String nome) {
+        List<Receita> items = this.buscar();
         return items.stream()
-                .filter(receita -> receita.getNome().equalsIgnoreCase(nome))
+                .filter(receita -> receita.getTitulo().equalsIgnoreCase(nome))
                 .collect(Collectors.toList());
     }
 
     public List<Receita> buscarPorAutor(String autor) {
+        List<Receita> items = this.buscar();
         return items.stream()
-                .filter(receita -> receita.getAutor().equalsIgnoreCase(autor))
+                .filter(receita -> receita.getAutor().getNome().equalsIgnoreCase(autor))
                 .collect(Collectors.toList());
     }
 
     public List<Receita> buscarPorAvaliacao(int avaliacao) {
+        List<Receita> items = this.buscar();
         return items.stream()
-                .filter(receita -> receita.getAvaliacao() == avaliacao)
+                .filter(receita -> receita.getAvaliacao().getNota() == (avaliacao))
                 .collect(Collectors.toList());
     }
 }
