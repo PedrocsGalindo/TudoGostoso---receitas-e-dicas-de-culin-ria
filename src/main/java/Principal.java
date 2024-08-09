3f1bbf2d636dd14795d70cd5350f06480da64911
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-09 11:26:05 -0300

    intancia controle assim agora e so precisa usar ele pra manipular os modelos

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,19 +1,18 @@
-import controle.ControleRepositorioUsuario;
-import controle.ControleUsuario;
+import controle.ControleFactory;
 import modelo.Usuario;
-import repositorio.RepositorioUsuario;
+import controle.Controle;
+
 
 
 public class Principal {
     public static void main(String[] args) {
-        ControleRepositorioUsuario controleRepositorioUsuario= new ControleRepositorioUsuario(new RepositorioUsuario());
-        ControleUsuario controleU = new ControleUsuario(controleRepositorioUsuario);
+        Controle controle = ControleFactory.criarControleGeral();
 
-        Usuario pedro = controleU.criarUsuario("pedro", "4315","pedrocgsouza23@gmail", "123141");
-        Usuario caio = controleU.criarUsuario("caio", "4315","pedrocgsouza23@gmail", "123141");
+        Usuario pedro = controle.criarUsuario("pedro", "4315","pedrocgsouza23@gmail", "123141");
+        Usuario caio = controle.criarUsuario("caio", "4315","pedrocgsouza23@gmail", "123141");
 
-        Usuario caioc = controleRepositorioUsuario.recuperarUsuarioPorId(1);
-        Usuario pedror= controleRepositorioUsuario.recuperarUsuarioPorId(0);
+        Usuario caioc = controle.recuperarUsuarioPorId(1);
+        Usuario pedror= controle.recuperarUsuarioPorId(0);
 
         System.out.println(pedror.getNome());
         System.out.println(caioc.getNome());
