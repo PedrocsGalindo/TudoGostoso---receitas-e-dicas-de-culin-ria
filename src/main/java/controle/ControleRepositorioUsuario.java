df2652a8c0320bae660bc3d545c7b67b87d6650b
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-09 11:04:01 -0300

    responsavel por verificar se ja existe o usuario

--- src/main/java/controle/ControleRepositorioUsuario.java
+++ src/main/java/controle/ControleRepositorioUsuario.java
@@ -1,33 +1,45 @@
 package controle;
 
+import exceptions.UsuarioJaExistenteException;
 import modelo.Usuario;
 import repositorio.RepositorioUsuario;
 import javax.mail.internet.InternetAddress;
 
-public class ControleRepositorioUsuario  {
+public class ControleRepositorioUsuario {
 
-    private static final RepositorioUsuario repositorio = new RepositorioUsuario();
+    private final RepositorioUsuario repositorio;
 
-    public static void salvarUsuario(Usuario usuario){
+    public ControleRepositorioUsuario(RepositorioUsuario repositorio){
+        this.repositorio = repositorio;
+
+    }
+
+    public  void salvarUsuario(Usuario usuario) throws UsuarioJaExistenteException {
+        if (repositorio.buscarPorEmail(usuario.getEmail()) != null){
+            throw new UsuarioJaExistenteException("Já existe uma conta com esse email");
+        }
+        if (repositorio.buscarPorCpf(usuario.getCpf()) != null){
+            throw new UsuarioJaExistenteException("Já existe uma conta com esse CPF");
+        }
         repositorio.salvar(usuario);
     }
 
-    public static void excluirUsuario(Usuario usuario){ repositorio.excluir(usuario);
+    public  void excluirUsuario(Usuario usuario){ repositorio.excluir(usuario);
     }
 
     //retornar o usuario baseado no id
-    public static Usuario recuperarUsuarioPorId(int id){
+    public  Usuario recuperarUsuarioPorId(int id){
         return repositorio.buscarPorId(id);
     }
 
     //retornar o usuario baseado no cpf
-    public static Usuario recuperarUsuarioPorCpf(String cpf){
+    public Usuario recuperarUsuarioPorCpf(String cpf){
         return repositorio.buscarPorCpf(cpf);
     }
 
     //retornar o usuario baseado no email
-    public static Usuario recuperarUsuarioPorEmail(InternetAddress email){
+    public Usuario recuperarUsuarioPorEmail(InternetAddress email){
         return repositorio.buscarPorEmail(email);
     }
 }
-//Precisa ser adicionado o CPF agora
+
