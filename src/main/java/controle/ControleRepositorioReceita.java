dc55508a92fa5f78983bcf2ee9a7fca81ed74f6e
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-09 11:26:30 -0300

    construtor, so para puder testar o factory

--- src/main/java/controle/ControleRepositorioReceita.java
+++ src/main/java/controle/ControleRepositorioReceita.java
@@ -1,4 +1,13 @@
 package controle;
 
+import repositorio.RepositorioReceita;
+
 public class ControleRepositorioReceita {
+
+    private final RepositorioReceita repositorioReceita;
+
+    public ControleRepositorioReceita(RepositorioReceita repositorioReceita) {
+        this.repositorioReceita = repositorioReceita;
+    }
+
 }
