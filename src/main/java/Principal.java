e82e9f5f6a0f27b69c28a85db7a7d4a8d9245bfa
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-09 09:45:15 -0300

    agora para intanciar um usuario tem que usar o controle Usuario, assim é melhor que fica a responsabilidade sobre Usuario em Controle Usario

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,16 +1,16 @@
 import controle.ControleRepositorioUsuario;
+import controle.ControleUsuario;
 import controle.Usuario;
 
 
 public class Principal {
     public static void main(String[] args) {
-        ControleRepositorioUsuario controle = new ControleRepositorioUsuario();
 
-        Usuario pedro = controle.criarESalvarUsuario("pedro", "4315","pedrocg", "123141");
-        Usuario caio = controle.criarESalvarUsuario("caio", "4315","pedrocg", "123141");
+        Usuario pedro = ControleUsuario.criarUsuario("pedro", "4315","pedrocgsouza23@gmail", "123141");
+        Usuario caio = ControleUsuario.criarUsuario("caio", "4315","pedrocgsouza23@gmail", "123141");
 
-        Usuario caioc = controle.recuperarUsuarioPorId(1);
-        Usuario pedror= controle.recuperarUsuarioPorId(0);
+        Usuario caioc = ControleRepositorioUsuario.recuperarUsuarioPorId(1);
+        Usuario pedror= ControleRepositorioUsuario.recuperarUsuarioPorId(0);
 
         System.out.println(pedror.getNome());
         System.out.println(caioc.getNome());
