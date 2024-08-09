1c2207a8a051c35c63c17c920c7dd58127a9ae26
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-09 09:46:01 -0300

    ControleUsuario

---
+++ src/main/java/controle/ControleUsuario.java
@@ -0,0 +1,27 @@
+package controle;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class ControleUsuario {
+
+    public static Usuario criarUsuario(String nome, String senha, String email, String cpf) throws NumberFormatException {
+        //validações
+        try {
+            InternetAddress emailAddress = new InternetAddress(email);
+            emailAddress.validate();
+
+            if (nome.isEmpty() || senha.isEmpty()|| email.isEmpty() || cpf.isEmpty()) {throw new NullPointerException();}
+
+
+            Usuario usuario = new Usuario(nome, senha, emailAddress,cpf);
+            ControleRepositorioUsuario.salvarUsuario(usuario);
+            return usuario;
+        } catch (AddressException e) {
+            System.out.println("email invalido");
+        } catch (NullPointerException e){
+            System.out.println("algum dos campos esta vazio");
+        }
+        return null;
+    }
+}
