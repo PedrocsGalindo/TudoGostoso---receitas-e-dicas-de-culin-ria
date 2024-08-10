4dc8e966fbb02e26c92eafd9009f271b7aeb8c44
Vinícius <142949993+ViniGBPl@users.noreply.github.com>
2024-08-10 14:54:46 -0300

    RepositórioListaDeCompras

--- src/main/java/repositorio/Mainteste.java
+++ src/main/java/repositorio/Mainteste.java
@@ -1,5 +1,6 @@
 package repositorio;
 
+import modelo.ListaDeCompras;
 import modelo.Receita;
 
 public class Mainteste {
@@ -7,14 +8,18 @@ public class Mainteste {
 
         Receita a = new Receita("inicio",null,null,null,null,null,null);
         Receita b = new Receita("inicio2",null,null,null,null,null,null);
-
+        ListaDeCompras a1 = new ListaDeCompras(4,"das",null,null);
         RepositorioReceita repositorioReceita =  new RepositorioReceita();
+        RepositorioListaDeCompras repositorioListaDeCompras = new RepositorioListaDeCompras();
 
         repositorioReceita.salvar(a);
         repositorioReceita.salvar(b);
+        repositorioListaDeCompras.salvar(a1);
+
 
 
         System.out.println( repositorioReceita.buscar());
+        System.out.println(repositorioListaDeCompras.buscar());
 
             
     }
---
+++ src/main/java/repositorio/RepositorioListaDeCompras.java
@@ -0,0 +1,12 @@
+package repositorio;
+
+import modelo.ListaDeCompras;
+
+public class RepositorioListaDeCompras extends RepositorioGenerico<ListaDeCompras> {
+
+   public RepositorioListaDeCompras( ){
+       super("src/main/resourcers/repositorios/RepositorioListaDeCompras/repositorio.ser");
+
+
+   }
+}
