64504d81638de4b55dc9da42f991dd0e0cb64ee4
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-13 01:20:55 -0300

    teste

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -8,13 +8,16 @@ public class Principal {
     public static void main(String[] args) {
         Controle controle = ControleFactory.criarControleGeral();
 
-        Usuario pedro = controle.criarUsuario("pedro", "4315","@gmail", "31321");
-        Usuario caio = controle.criarUsuario("caio", "4315","@gmail", "123141");
-        Usuario joaquim = controle.criarUsuario("Joaquim", "senha", "@gmail","3214");
+        Usuario pedro = controle.criarUsuario("pedro", "4315","pedrocgsouza23@gmail", "31321");
+        Usuario caio = controle.criarUsuario("caio", "4315","pedrocgsouza4fc@gmail", "123141");
+        Usuario joaquim = controle.criarUsuario("Joaquim", "senha", "cesarpedrog23@gmail","3214");
 
         Usuario caioc = controle.recuperarUsuarioPorId(1);
         Usuario pedror= controle.recuperarUsuarioPorId(2);
         Usuario joaquimm = controle.recuperarUsuarioPorId(3);
+
+        controle.criarIngrediente("batata");
+        controle.criarIngrediente("cenoura");
 
 
         System.out.println(caioc.getNome());
