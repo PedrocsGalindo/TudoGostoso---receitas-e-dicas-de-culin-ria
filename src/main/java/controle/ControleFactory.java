34658f586e4184a0edafb602432c7f2b2fba925b
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-13 00:50:38 -0300

    Criando Controle com os novos controles

--- src/main/java/controle/ControleFactory.java
+++ src/main/java/controle/ControleFactory.java
@@ -1,5 +1,6 @@
 package controle;
 
+import repositorio.RepositorioIngrediente;
 import repositorio.RepositorioReceita;
 import repositorio.RepositorioUsuario;
 
@@ -8,10 +9,12 @@ public class ControleFactory {
     public static Controle criarControleGeral() {
         ControleRepositorioUsuario controleRepositorioUsuario = new ControleRepositorioUsuario(new RepositorioUsuario());
         ControleRepositorioReceita controleRepositorioReceita = new ControleRepositorioReceita(new RepositorioReceita());
+        ControleRepositorioIngrediente controleRepositorioIngrediente = new ControleRepositorioIngrediente(new RepositorioIngrediente());
         ControleUsuario controleUsuario = new ControleUsuario(controleRepositorioUsuario);
         ControleReceita controleReceita = new ControleReceita(controleRepositorioReceita);
         ControleUsuarioChef controleUsuarioChef = new ControleUsuarioChef(controleRepositorioUsuario, controleReceita);
+        ControleIngrediente controleIngrediente = new ControleIngrediente(controleRepositorioIngrediente);
 
-        return new Controle(controleRepositorioUsuario, controleRepositorioReceita, controleUsuario, controleReceita, controleUsuarioChef);
+        return new Controle(controleRepositorioUsuario, controleRepositorioReceita, controleRepositorioIngrediente, controleUsuario, controleReceita, controleUsuarioChef, controleIngrediente);
     }
 }
