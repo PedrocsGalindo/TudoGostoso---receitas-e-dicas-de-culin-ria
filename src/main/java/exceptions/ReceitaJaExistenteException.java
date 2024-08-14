cb5a680f02e2e6bf04cfdde4b7bbcbb47e96e212
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-14 14:42:48 -0300

    ReceitaJaExistenteException

---
+++ src/main/java/exceptions/ReceitaJaExistenteException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ReceitaJaExistenteException extends Exception {
+    public ReceitaJaExistenteException(String msg) {
+        super(msg);
+    }
+    public ReceitaJaExistenteException(String msg, Throwable causa) {
+        super(msg, causa);
+    }
+}
