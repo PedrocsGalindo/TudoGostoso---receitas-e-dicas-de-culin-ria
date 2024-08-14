9bec5dfa1ed0df0e728fedee9dd1bc8de3c65664
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-14 14:29:49 -0300

    verifica se a Receita Existe e getLastId

--- src/main/java/controle/ControleReceita.java
+++ src/main/java/controle/ControleReceita.java
@@ -1,5 +1,6 @@
 package controle;
 
+import exceptions.ReceitaJaExistenteException;
 import modelo.Receita;
 
 public class ControleReceita {
@@ -10,9 +11,17 @@ public class ControleReceita {
         this.controleRepositorioReceita = controleRepositorioReceita;
     }
 
-    public Receita cadastrarReceita(Receita receita) {
-        controleRepositorioReceita.salvarReceita(receita);
+    public Receita cadastrarReceita(Receita receita) throws ReceitaJaExistenteException{
+        if(controleRepositorioReceita.buscarReceitaPorAutorETitulo(receita.getAutor(), receita.getTitulo()) == null) {
+            controleRepositorioReceita.salvarReceita(receita);
+        }else {
+            throw new ReceitaJaExistenteException("Usuario "+ receita.getAutor().getNome() + " já possui receita com o titulo " + receita.getTitulo());
+        }
         return receita;
+    }
+
+    public int getLastId(){
+        return controleRepositorioReceita.getLastId();
     }
 
 }
