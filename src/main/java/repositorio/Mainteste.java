d19f9036e26fdf780efb2c0aa54086fa4eea1308
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-14 14:40:04 -0300

    .

--- src/main/java/repositorio/Mainteste.java
+++ src/main/java/repositorio/Mainteste.java
@@ -7,21 +7,6 @@ import modelo.Usuario;
 public class Mainteste {
     public static void main(String[] args) {
 
-        Receita a = new Receita("inicio",null,null,null,null,null,null);
-        Receita b = new Receita("inicio2",null,null,null,null,null,null);
-
-
-        RepositorioUsuario repositorioUsuario = new RepositorioUsuario();
-        RepositorioReceita repositorioReceita =  new RepositorioReceita();
-
-
-        repositorioReceita.salvar(a);
-        repositorioReceita.salvar(b);
-
-
-        System.out.println( repositorioReceita.buscar());
-
-            
     }
 
 
