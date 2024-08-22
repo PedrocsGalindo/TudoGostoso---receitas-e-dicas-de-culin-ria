a8d2c534634cba8a94ab20626675ef96011e2d07
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-22 20:21:39 -0300

    Removido RepositorioIDS e ControleRepositorioIDs.java

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -37,13 +37,6 @@ public class Usuario implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return id == usuario.id;
-    }
-    @Override
     public int hashCode() {
         return Objects.hash(id, cpf);
     }
