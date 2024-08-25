5397ea118f57fc3cac4539cb09f8a9fb617283c5
Vinícius <142949993+ViniGBPl@users.noreply.github.com>
2024-08-25 18:35:57 -0300

    ControllerPerfil

---
+++ src/main/java/org/tudogostoso/fxcontroller/FxPerfilController.java
@@ -0,0 +1,29 @@
+package org.tudogostoso.fxcontroller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FxPerfilController {
+
+    @FXML
+    private Button BTNListaDeCompras;
+
+    @FXML
+    private Button BTNMinhasReceitasFavoritas;
+
+    @FXML
+    private Button BTNVoltar;
+
+    @FXML
+    private Label LabelCPF;
+
+    @FXML
+    private Label LabelD;
+
+    @FXML
+    private Label LabelEmail;
+
+    @FXML
+    private Label LabelNomeDeUsuario;
+
+}
