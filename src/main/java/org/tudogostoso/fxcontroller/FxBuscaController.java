c00bcf1e62190264b6cd88b8ffad38189d4321f3
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-25 13:54:50 -0300

    checkboxs

--- src/main/java/org/tudogostoso/fxcontroller/FxBuscaController.java
+++ src/main/java/org/tudogostoso/fxcontroller/FxBuscaController.java
@@ -1,50 +1,88 @@
 package org.tudogostoso.fxcontroller;
 
+import org.tudogostoso.modelo.Receita;
+import org.tudogostoso.controle.Controle;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class FxBuscaController {
 
     @FXML
-    private TextField TextFildBusca;
+    private TextField textFildBusca;
 
     @FXML
-    private CheckBox CheckBoxPorNome;
+    private CheckBox checkBoxPorNome;
 
     @FXML
-    private CheckBox CheckBoxPorAutor;
+    private CheckBox checkBoxPorAutor;
 
     @FXML
-    private CheckBox CheckBoxPorIngrediente;
+    private CheckBox checkBoxPorIngrediente;
 
     @FXML
-    private CheckBox CheckBoxPorAvaliacao;
+    private CheckBox checkBoxPorAvaliacao;
 
     @FXML
-    private TextArea TextArea1;
+    private TextArea textArea1;
 
     @FXML
     private ImageView ImageView1;
 
     @FXML
-    private ImageView ImageView2;
+    private ImageView imageView2;
 
     @FXML
-    private TextArea TextArea2;
+    private TextArea textArea2;
 
     @FXML
-    private ImageView ImageView3;
+    private ImageView imageView3;
 
     @FXML
-    private TextArea TextArea3;
+    private TextArea textArea3;
 
     @FXML
-    private ImageView ImageView4;
+    private ImageView imageView4;
 
     @FXML
-    private TextArea TextArea4;
+    private TextArea textArea4;
+
+    @FXML
+    void cliqueAutor(MouseEvent event) {
+        checkBoxPorAutor.setSelected(true);
+        checkBoxPorNome.setSelected(false);
+        checkBoxPorIngrediente.setSelected(false);
+        checkBoxPorAvaliacao.setSelected(false);
+
+    }
+
+    @FXML
+    void cliqueAvaliacao(MouseEvent event) {
+        checkBoxPorAvaliacao.setSelected(true);
+        checkBoxPorNome.setSelected(false);
+        checkBoxPorIngrediente.setSelected(false);
+        checkBoxPorAutor.setSelected(false);
+    }
+
+    @FXML
+    void cliqueIngrediente(MouseEvent event) {
+        checkBoxPorIngrediente.setSelected(true);
+        checkBoxPorNome.setSelected(false);
+        checkBoxPorAutor.setSelected(false);
+        checkBoxPorAvaliacao.setSelected(false);
+    }
+
+    @FXML
+    void cliqueNome(MouseEvent event) {
+        checkBoxPorNome.setSelected(true);
+        checkBoxPorAutor.setSelected(false);
+        checkBoxPorIngrediente.setSelected(false);
+        checkBoxPorAvaliacao.setSelected(false);
+
+    }
 
 }
