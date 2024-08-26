8ab287317e871703789a67658067240b69c22d98
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-26 08:41:14 -0300

    Abrir explorador de arquivos para adicionar a imagem da receita, aninda precisa ser feito toda a logica de adicionar imagem

---
+++ src/main/java/org/tudogostoso/fxcontroller/ControllerCriarReceitas.java
@@ -0,0 +1,39 @@
+package org.tudogostoso.fxcontroller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+
+public class ControllerCriarReceitas {
+
+    @FXML
+    private Button arquivos;
+
+    @FXML
+    void arquivosHandle() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Selecione um Arquivo");
+
+        // Filtrar apenas para arquivos de imagem
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Imagens", "*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif")
+        );
+
+        // Abre o FileChooser e obtém o arquivo selecionado
+        Stage stage = (Stage) arquivos.getScene().getWindow(); // Obtém a janela atual
+        File caminhoArquivoUsuario = fileChooser.showOpenDialog(stage);
+
+        if (caminhoArquivoUsuario != null) {
+
+
+            System.out.println("Arquivo selecionado: " + caminhoArquivoUsuario.getAbsolutePath());
+            // Aqui você pode manipular o arquivo, por exemplo, carregá-lo em um ImageView
+        }
+    }
+
+}
+
