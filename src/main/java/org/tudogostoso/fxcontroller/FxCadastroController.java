d822b40bde5f9cabf86639961b67777a88e0d8a7
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-08-29 10:29:05 -0300

    Update FxCadastroController.java
    att controller cadastro

--- src/main/java/org/tudogostoso/fxcontroller/FxCadastroController.java
+++ src/main/java/org/tudogostoso/fxcontroller/FxCadastroController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
-public class FxCadastroController {
+public class TelaCadastroController {
 
     @FXML
     private TextField campoUsuario;
@@ -18,15 +18,39 @@ public class FxCadastroController {
     @FXML
     private Button botaoCadastro;
 
+    private String tipoUsuario = "Usuário Padrão"; // Por padrão, o tipo de usuário é "Usuário Padrão"
+
     @FXML
     private void cadastrarUsuario() {
         String usuario = campoUsuario.getText();
         String email = campoEmail.getText();
         String senha = campoSenha.getText();
 
-        mostrarAlerta(AlertType.INFORMATION, "Cadastro Realizado", 
-                      "Usuário: " + usuario + "\nEmail: " + email + "\nSenha: " + senha);
+        mostrarAlerta(AlertType.INFORMATION, "Cadastro Realizado",
+                "Usuário: " + usuario + "\nEmail: " + email + "\nSenha: " + senha + "\nTipo de Usuário: "
+                        + tipoUsuario);
         limparCampos();
+    }
+
+    @FXML
+    private void selecionarUsuarioPadrao() {
+        tipoUsuario = "Usuário Padrão";
+        mostrarAlerta(AlertType.INFORMATION, "Tipo de Usuário Selecionado", "Você selecionou: Usuário Padrão");
+    }
+
+    @FXML
+    private void selecionarUsuarioChef() {
+        tipoUsuario = "Usuário Chef";
+        mostrarAlerta(AlertType.INFORMATION, "Tipo de Usuário Selecionado", "Você selecionou: Usuário Chef");
+    }
+
+    @FXML
+    private void voltarParaLogin() {
+        try {
+            AppPrincipal.mudarTela("TelaLogin.fxml"); // Chama a troca para a tela de login
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void mostrarAlerta(AlertType tipoAlerta, String titulo, String mensagem) {
