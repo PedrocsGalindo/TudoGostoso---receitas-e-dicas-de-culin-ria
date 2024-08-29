09aed02b57760e40bae37ec5dc422c8d2aa908cc
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-29 09:33:21 -0300

    tela login navegavel

--- src/main/java/org/tudogostoso/fxcontroller/FxLoginController.java
+++ src/main/java/org/tudogostoso/fxcontroller/FxLoginController.java
@@ -1,11 +1,22 @@
+package org.tudogostoso.fxcontroller;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import org.tudogostoso.controle.Controle;
+import org.tudogostoso.controle.ControleFactory;
 
-public class TelaLoginController {
+public class FxLoginController {
 
     @FXML
     private TextField campoUsuario;
@@ -16,29 +27,46 @@ public class TelaLoginController {
     @FXML
     private Button botaoIrParaCadastro;
 
+    private static Controle controle = ControleFactory.criarControleGeral();
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
-    private void fazerLogin() {
+    void fazerLogin(ActionEvent event)  {
         String usuario = campoUsuario.getText();
         String senha = campoSenha.getText();
 
         // Simulação de verificação de login
         if ("admin".equals(usuario) && "1234".equals(senha)) {
             mostrarAlerta(AlertType.INFORMATION, "Login Sucesso", "Bem-vindo, " + usuario + "!");
+            mudarTela("/org/tudogostoso/telas/feed.fxml", event);
         } else {
             mostrarAlerta(AlertType.ERROR, "Falha no Login", "Usuário ou senha incorretos.");
         }
 
         limparCampos();
     }
-
     @FXML
-    private void irParaTelaCadastro() {
+    void irParaTelaCadastro(ActionEvent event) {
+        mudarTela("/org/tudogostoso/telas/cadastro.fxml", event);
+
+    }
+    private void mudarTela(String tela, Event evento) {
         try {
-            AppPrincipal.mudarTela("TelaCadastro.fxml");  // Chama a troca para a tela de cadastro
+            root = FXMLLoader.load(getClass().getResource(tela));
+            scene = new Scene(root);
+
+            // Obtenha a Stage a partir do evento
+            stage = (Stage) ((Node) evento.getSource()).getScene().getWindow();
+
+            stage.setScene(scene);
+            stage.show();;  // Chama a troca para a tela de cadastro ou feed
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 
     private void mostrarAlerta(AlertType tipoAlerta, String titulo, String mensagem) {
         Alert alerta = new Alert(tipoAlerta);
