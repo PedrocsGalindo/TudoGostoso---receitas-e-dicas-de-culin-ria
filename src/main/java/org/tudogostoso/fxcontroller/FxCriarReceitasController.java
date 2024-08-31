ee9632542f4b424260af16389d6199b1811d87a4
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-08-31 13:42:27 -0300

    Criar receitas

--- src/main/java/org/tudogostoso/fxcontroller/FxCriarReceitasController.java
+++ src/main/java/org/tudogostoso/fxcontroller/FxCriarReceitasController.java
@@ -2,16 +2,48 @@ package org.tudogostoso.fxcontroller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.event.ActionEvent;
 
 import java.io.File;
 
 
-public class FxControllerCriarReceitas {
+public class FxCriarReceitasController {
 
     @FXML
     private Button arquivos;
+
+    @FXML
+    private ImageView imagemEscolhida;
+
+    @FXML
+    private ChoiceBox<?> choicheBoxIngrediente, choiceBoxUnidadeDeMedida;
+
+    @FXML
+    private TextField textFieldQuantidade, textFieldPreparo, textFieldTitulo, textFieldTempodDePreapro, textFieldNomeIngrediente;
+
+    @FXML
+    private TextArea textAreaItemnsIngredientes;
+
+    @FXML
+    void HandllerButtonVoltar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handllerButtonAdicionarItemIngrediente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handllerButtonCriarIngrediente(ActionEvent event) {
+
+    }
 
     @FXML
     void arquivosHandle() {
@@ -31,6 +63,9 @@ public class FxControllerCriarReceitas {
             // logica para salvar o caminho do arquivo
         }
     }
+    @FXML
+    void handllerButtonCriarReceita(ActionEvent event) {
 
+    }
 }
 
