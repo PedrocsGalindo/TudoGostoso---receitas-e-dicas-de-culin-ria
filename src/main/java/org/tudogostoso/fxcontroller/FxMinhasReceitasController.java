f6831ca4f06ef6b6a8d9a8cb6bb46ca820f9db03
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-09-01 21:12:06 -0300

    Classe da Tela Minhas receitas adicionado, mas sem corpo

---
+++ src/main/java/org/tudogostoso/fxcontroller/FxMinhasReceitasController.java
@@ -0,0 +1,36 @@
+package org.tudogostoso.fxcontroller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class FxMinhasReceitasController {
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private ListView<?> listViewMinhasReceitas;
+
+    @FXML
+    private Button botaoAdicionarReceita;
+
+    @FXML
+    private Button botaoExcluirReceita;
+
+    @FXML
+    void handleBotaoAdicionarReceita(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleBotaoExcluirReceita(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleBotaoVoltar(ActionEvent event) {
+
+    }
+}
