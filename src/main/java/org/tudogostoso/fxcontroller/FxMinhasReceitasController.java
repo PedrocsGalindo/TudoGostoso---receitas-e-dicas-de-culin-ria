292880075d2b24dea3b57e6f14ef0633d352e1f3
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-09-02 10:16:40 -0300

    Minhas Receitas funcional

--- src/main/java/org/tudogostoso/fxcontroller/FxMinhasReceitasController.java
+++ src/main/java/org/tudogostoso/fxcontroller/FxMinhasReceitasController.java
@@ -4,46 +4,91 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
 import org.tudogostoso.controle.Controle;
 import org.tudogostoso.controle.ControleFactory;
 import org.tudogostoso.modelo.Receita;
 import org.tudogostoso.modelo.Sessao;
 import org.tudogostoso.modelo.Usuario;
 
+import java.io.File;
+
 public class FxMinhasReceitasController {
 
+    @FXML
+    private ListView<Receita> listViewMinhasReceitas;
 
     private ObservableList<Receita> minhasReceitasList;
-    private Controle controle = ControleFactory.criarControleGeral();
-    private FxGerenciadorTelas gerenciadorTelas = FxGerenciadorTelas.getInstance();
+    private final Controle controle = ControleFactory.criarControleGeral();
+    private final FxGerenciadorTelas gerenciadorTelas = FxGerenciadorTelas.getInstance();
     private Usuario usuario = Sessao.getUsuarioSessao();
 
     @FXML
     public void initialize() {
         minhasReceitasList = FXCollections.observableArrayList();
         listViewMinhasReceitas.setItems(minhasReceitasList);
+        listViewMinhasReceitas.setCellFactory(new Callback<ListView<Receita>, ListCell<Receita>>() {
+            @Override
+            public ListCell<Receita> call(ListView<Receita> param) {
+                return new FxMinhasReceitasController.ReceitaListCell();
+            }
+        });
         carregarMinhasReceitas();
+
+
     }
+    public class ReceitaListCell extends ListCell<Receita> {
+        private HBox hBoxReceita;
+        private VBox vBoxReceita;
+        private ImageView imageViewReceita;
+        private Text textReceitaTitulo, textReceitaCategoriaENota;
+
+        public ReceitaListCell() {
+            super();
+            imageViewReceita = new ImageView();
+            imageViewReceita.setFitHeight(50);
+            imageViewReceita.setFitWidth(50);
+            textReceitaTitulo = new Text();
+            textReceitaTitulo.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+            textReceitaCategoriaENota = new Text();
+            textReceitaCategoriaENota.setStyle("-fx-font-size: 12px; -fx-text-fill: gray;");
+
+            vBoxReceita = new VBox(textReceitaTitulo, textReceitaCategoriaENota);
+            vBoxReceita.setSpacing(5);
+            hBoxReceita = new HBox(imageViewReceita, vBoxReceita);
+            hBoxReceita.setSpacing(10);
+        }
+        @Override
+        protected void updateItem(Receita item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null && !empty) {
+                textReceitaTitulo.setText(item.getTitulo());
+                textReceitaCategoriaENota.setText("Categoria: " + item.getCategoria() + "\n" + "Nota: " + String.format("%.1f", (double) item.getNota()));
+
+                File file = new File(item.getCaminhoImagem());
+                Image image;
+                if (file.exists()) {
+                    image = new Image(file.toURI().toString(), 50, 50, false, true);
+                } else {
+                    image = new Image(getClass().getResourceAsStream("/org/tudogostoso/Imagens/fotoDefaultReceitas.jpg"), 50, 50, false, true);
+                }
+                imageViewReceita.setImage(image);
+                setGraphic(hBoxReceita);
+            } else {
+                setGraphic(null);
+            }
+        }
+    }
+
     private void carregarMinhasReceitas() {
         minhasReceitasList.setAll(controle.buscarReceitaPorAutor(usuario));
     }
-    
-    @FXML
-    private Button botaoVoltar;
-
-    @FXML
-    private ListView<Receita> listViewMinhasReceitas;
-
-    @FXML
-    private Button botaoAdicionarReceita;
-
-    @FXML
-    private Button botaoExcluirReceita;
-
-
 
     @FXML
     void handleBotaoAdicionarReceita(ActionEvent event) {
@@ -52,7 +97,7 @@ public class FxMinhasReceitasController {
     }
 
     @FXML
-    void handleBotaoExcluirReceita(ActionEvent event) {
+    void handleBotaoExcluirReceita() {
         // Obtém a receita selecionada na ListView
         Receita receitaSelecionada = listViewMinhasReceitas.getSelectionModel().getSelectedItem();
 
