edb5a40b608ae48f4c601a36c0c84dcf8a4d4120
Pedrocesargs <pedrocsgalindo@gmail.com>
2024-09-04 20:31:38 -0300

    populando

--- src/main/java/org/tudogostoso/repositorios/RepositorioImagens.java
+++ src/main/java/org/tudogostoso/repositorios/RepositorioImagens.java
@@ -12,28 +12,24 @@ public class RepositorioImagens {
 
     public String salvar(File arquivo, String nomeArquivo) throws IOException{
 
-        // Cria um ImageInputStream a partir do arquivo de imagem
         ImageInputStream iis = ImageIO.createImageInputStream(arquivo);
 
-        // Obtém o iterador de ImageReaders que podem ler o arquivo
         Iterator<ImageReader> iteradores = ImageIO.getImageReaders(iis);
 
         if (!iteradores.hasNext()) {
             throw new IOException("problema com o formato da imagem");
         }
 
-        // Obtém o primeiro ImageReader disponível
         ImageReader leitor = iteradores.next();
 
         // Obtém o formato do arquivo de imagem
         String formatoDeImagem = leitor.getFormatName();
 
-        // Fecha o ImageInputStream
         iis.close();
 
         // caminho original do arquivo
         BufferedImage imagem = ImageIO.read(arquivo);
-        // Earquivo de destino
+
         File arquivoSaida = new File("src/main/resources/org/tudogostoso/ImagensReceitas/" + nomeArquivo + "." + formatoDeImagem);
 
         // Salva a imagem no formato desejado
