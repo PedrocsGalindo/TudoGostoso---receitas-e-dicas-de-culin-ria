6e977c897cdc90eadb70a885addeb7989e4ed05c
Vinícius <142949993+ViniGBPl@users.noreply.github.com>
2024-09-08 21:36:04 -0300

    Estilização

--- src/main/java/org/tudogostoso/modelo/GoogleImagens.java
+++ src/main/java/org/tudogostoso/modelo/GoogleImagens.java
@@ -6,15 +6,25 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GoogleImagens {
     private static final String API_KEY = "106151324710272841288";
-    private static final String CX ="702669bb08f8e4a1f";
+    private static final String CX = "702669bb08f8e4a1f";
 
-    // Método para buscar imagens no Google
-    public static String buscarImagem(String consulta) throws Exception {
+    /**
+     * Busca imagens no Google Custom Search API.
+     *
+     * @param consulta O termo de pesquisa para buscar imagens.
+     * @param numImagens O número de imagens a ser retornado.
+     * @return Uma lista de URLs de imagens.
+     * @throws Exception Se houver um erro durante a chamada da API.
+     */
+    public static List<String> buscarImagens(String consulta, int numImagens) throws Exception {
+        // Construa a URL com o número de imagens especificado
         String urlString = "https://www.googleapis.com/customsearch/v1?q=" + consulta +
-                "&cx=" + CX + "&key=" + API_KEY + "&searchType=image&num=1";
+                "&cx=" + CX + "&key=" + API_KEY + "&searchType=image&num=" + numImagens;
         URL url = new URL(urlString);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -30,15 +40,18 @@ public class GoogleImagens {
         in.close();
         con.disconnect();
 
-        // Parseando o JSON retornado
         JSONObject json = new JSONObject(content.toString());
         JSONArray items = json.getJSONArray("items");
-        if (items.length() > 0) {
-            // Retorna o link da primeira imagem encontrada
-            String linkImagem = items.getJSONObject(0).getString("link");
-            return linkImagem;
-        } else {
+
+        List<String> linksImagens = new ArrayList<>();
+        for (int i = 0; i < items.length(); i++) {
+            linksImagens.add(items.getJSONObject(i).getString("link"));
+        }
+
+        if (linksImagens.isEmpty()) {
             throw new Exception("Nenhuma imagem encontrada");
         }
+
+        return linksImagens;
     }
 }
